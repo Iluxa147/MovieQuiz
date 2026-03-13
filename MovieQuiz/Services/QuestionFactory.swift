a9dcecc170cbd71d535f3cd8df5230138d76b257
Iluxa147 <pokolewIlja@gmail.com>
2026-03-13 14:16:45 +0300

    feat: making questions more diverse with random rating and type

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -8,70 +8,52 @@
 import Foundation
 
 final class QuestionFactory: QuestionFactoryProtocol {
-    // MARK: - Mock
-    /*
-     private let questions: [QuizQuestion] = [
-     QuizQuestion(
-     imageName: "The Godfather",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "The Dark Knight",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "Kill Bill",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "The Avengers",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "Deadpool",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "The Green Knight",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: true),
-     QuizQuestion(
-     imageName: "Old",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: false),
-     QuizQuestion(
-     imageName: "The Ice Age Adventures of Buck Wild",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: false),
-     QuizQuestion(
-     imageName: "Tesla",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: false),
-     QuizQuestion(
-     imageName: "Vivarium",
-     text: "Рейтинг этого фильма больше чем 6?",
-     correctAnswer: false)
-     ]
-     */
+    // MARK: - State
     
-    // MARK: - Private fields
+    private enum QuestionType: String, CaseIterable {
+        case lesser = "меньше"
+        case bigger = "больше"
+        
+    }
+    
+    private enum Constants {
+        static let defaultRatingThreshold: Float = 7
+        static let defaultQuestionType = QuestionType.bigger
+        static let ratingRangeForRandom = 1...9
+        static let randomRatingTermBound: Float = 0.5
+        static let randomRatingTermStep: Float = 0.1
+    }
+    
+    private var randomRatingTerm: Float {
+        let index = (0..<moviesRatingRandomTerms.count).randomElement() ?? 0
+        return moviesRatingRandomTerms[safe: index] ?? Constants.randomRatingTermStep
+    }
+    
     private let moviesLoader: MoviesLoadingProtocol
     private weak var delegate: QuestionFactoryDelegate?
     private var movies: [MostPopularMovie] = []
+    private var moviesRatingRandomTerms: [Float] = []
     
     // MARK: - Public members
     
     init(moviesLoader: MoviesLoadingProtocol, delegate: QuestionFactoryDelegate?) {
         self.moviesLoader = moviesLoader
         self.delegate = delegate
+        
+        for val in stride(from: -Constants.randomRatingTermBound,
+                          to: Constants.randomRatingTermBound,
+                          by: Constants.randomRatingTermStep) {
+            moviesRatingRandomTerms.append(Float(val))
+        }
     }
     
     // MARK: - QuestionFactoryProtocol
     func requestNextQuestion() {
         DispatchQueue.global().async { [weak self] in
             guard let self else { return }
-            let index = (0..<self.movies.count).randomElement() ?? 0
+            guard !self.movies.isEmpty else { return }
             
+            let index = (0..<self.movies.count).randomElement() ?? 0
             guard let movie = self.movies[safe: index] else { return }
             
             var imageData = Data()
@@ -81,12 +63,7 @@ final class QuestionFactory: QuestionFactoryProtocol {
                 print("Failed to load image")
             }
             
-            let text = "Рейтинг этого фильма больше чем 7?"
-            let rating = Float(movie.rating) ?? 0
-            let correctAnswer = rating > 7
-            let question = QuizQuestion(image: imageData,
-                                        text: text,
-                                        correctAnswer: correctAnswer)
+            let question = CreateQuestion(imageData: imageData, movieRating: movie.rating)
             
             DispatchQueue.main.async { [weak self] in
                 guard let self else { return }
@@ -109,5 +86,40 @@ final class QuestionFactory: QuestionFactoryProtocol {
                 }
             }
         }
+    }
+    
+    // MARK: - Private members
+    
+    private func CreateQuestion(imageData: Data,
+                                actualMovieRating: Float,
+                                questionType: QuestionType = Constants.defaultQuestionType,
+                                ratingForQuestion: Float = Constants.defaultRatingThreshold) -> QuizQuestion {
+        var correctAnswer: Bool
+        switch questionType {
+        case .lesser:
+            correctAnswer = actualMovieRating < ratingForQuestion
+        default:
+            correctAnswer = actualMovieRating > ratingForQuestion
+        }
+        
+        let text = "Рейтинг этого фильма \(questionType.rawValue) чем \(String(format: "%.1f", ratingForQuestion))?"
+        
+        return QuizQuestion(image: imageData, text: text, correctAnswer: correctAnswer)
+    }
+    
+    private func CreateQuestion(imageData: Data, movieRating: String) -> QuizQuestion {
+        let actualMovieRating = Float(movieRating) ?? 0
+        if actualMovieRating.isZero || actualMovieRating < 0 {
+            return CreateQuestion(imageData: imageData, actualMovieRating: actualMovieRating)
+        }
+        
+        let randomQuestionType = QuestionType.allCases.randomElement() ?? Constants.defaultQuestionType
+        let ratingForQuestion = max(Float(Constants.ratingRangeForRandom.lowerBound),
+                                    min(actualMovieRating + Float(randomRatingTerm), Float(Constants.ratingRangeForRandom.upperBound)))
+        
+        return CreateQuestion(imageData: imageData,
+                              actualMovieRating: actualMovieRating,
+                              questionType: randomQuestionType,
+                              ratingForQuestion: ratingForQuestion)
     }
 }
