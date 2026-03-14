fe17b9dafa105ffc87ecb11ba9d3da478a3c6ff4
Iluxa147 <pokolewIlja@gmail.com>
2026-03-14 03:33:36 +0300

    test: array and MoviesLoader were added

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArrayTests.swift
+//  MovieQuiz
+//
+//  Created by Ilya Pokolev on 14.03.2026.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    }
+}
